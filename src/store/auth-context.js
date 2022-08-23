7864dcb22491b86266394be23f947fa7b5ecb18a
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-23 11:14:51 +0300

    persisting the authentication state

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -10,14 +10,17 @@ const AuthContext = React.createContext({
 export default AuthContext;
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextValue = {
